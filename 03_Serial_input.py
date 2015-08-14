d9c1a0711f1431a53951d062bd054aa7608c21fc
Rémi Bèges <remi.beges@gmail.com>
2015-08-14 18:21:28 +0200

    basic scripts running only serial or serial+protocol

--- 03_Serial_input.py
+++ 03_Serial_input.py
@@ -8,27 +8,23 @@ from API.SerialPort import SerialPort
 
 class SerialMgr():
     def __init__(self):
-        # Create protocol and give callback function
-        self.protocol = Protocol(self.on_new_payload)
-    
+            
         # Create serial port manager
         self.serial = SerialPort(self.on_rx_data)
         self.serial.connect("COM5",115200)
-        self.serial.run()
+        self.serial.start()
 
     def on_rx_data(self,c):
-        print(c)
-        self.protocol.decode(c)
-
-    def on_new_payload(self,payload):
-        print("decoded :",payload)
-        
+        print(c)        
 
 if __name__ == '__main__':
     
     mgr = SerialMgr()
+   
 
     while(mgr.serial.running):
+        #var = input("Type serial:")
+        #mgr.serial.write(var.encode())
         pass
     
     print("Done.")
