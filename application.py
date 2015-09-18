bf47451256a4d4265e52a5dafbf8a30e0729e3d0
Rémi Bèges <remi.beges@gmail.com>
2015-09-18 14:50:21 +0200

    Added serial health frame in app + calling model update regularly

--- application.py
+++ application.py
@@ -5,14 +5,16 @@ import tkinter as Tk
 import tkinter.ttk as ttk
 
 from API.Model import Model
-
+from API.TimingUtils import timeit
 from UI.COM_Frame import COM_Frame
 from UI.VariableTable_Frame import VariableTable_Frame
+from UI.SerialHealth_Frame import SerialHealth_Frame
 
 class Application(ttk.Frame):
+    @timeit
     def __init__(self,parent,**kwargs):
         # Init
-        self.parent = parent
+        self.root = parent
         ttk.Frame.__init__(self,parent,**kwargs)
 
         # Init configuration
@@ -24,17 +26,30 @@ class Application(ttk.Frame):
         # Create Model
         self.model = Model()
 
-        # Init tabs
+        # Init COM port frame
         self.com_ports = COM_Frame(self,self.model,relief=Tk.GROOVE)
         self.com_ports.grid(column=0,row=0,sticky='NSEW',pady=2,padx=5)
-        self.com_ports.refresh_COM_ports()
 
-        # Init tabs
+
+        # Init table frame
         self.var_table = VariableTable_Frame(self,self.model,relief=Tk.GROOVE)
         self.var_table.grid(column=0,row=1,sticky='NSEW',pady=2,padx=5)
 
+        # Init serial health Frame
+        self.health_frame = SerialHealth_Frame(self,self.model,self.root,relief=Tk.GROOVE)
+        self.health_frame.grid(column=0,row=2,sticky='NSEW',pady=2,padx=5)
+
+        self.com_ports.refresh_COM_ports()
+        self.update()
+
     def stop(self):
         self.model.finish()
+
+    @timeit
+    def update(self):
+        # Decode 100 instructions max
+        self.model.update(10)
+        self.root.after(10,self.update)
 
 
 
