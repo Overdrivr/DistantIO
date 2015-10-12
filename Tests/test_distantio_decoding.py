f21b4a4831a80fcf5dc26fa7212b1c96f0a9922f
Rémi Bèges <remi.beges@gmail.com>
2015-10-12 14:12:24 +0200

    Updated tests for new distantio protocol version

--- Tests/test_distantio_decoding.py
+++ Tests/test_distantio_decoding.py
@@ -10,29 +10,31 @@ import pytest
 # test decoding unkown command
 
 # test decoding valid returned-descriptor frame
-def test_decode_0x00():
+def test_returned_descriptor_variable_non_writeable():
     distantio = distantio_protocol()
 
     # inputs
     var_id = 999
     group_id = 18
-    name = "testvar"
+    name = "testvariable 1"
     fmt = 3
 
     # formatting
-    formatted_name = "{:8s}".format(name)
-    v = (var_id + (group_id << 10)).to_bytes(2,byteorder='big')
-    n = bytes(formatted_name,'ascii')
+    formatted_name = "{:14s}".format(name)
+    data_ID = (var_id + (group_id << 10)).to_bytes(2,byteorder='big')
+    data = bytes(formatted_name,'ascii')
 
     frame = bytearray()
     frame.append(0x00)
-    frame += v
+    frame += data_ID
     frame.append(fmt)
-    frame += n
+    frame += data
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
 
     frame += crc
+
+    # Testing frame
     response = distantio.process(frame)
 
     assert response['type'] == 'returned-descriptor'
@@ -42,16 +44,16 @@ def test_decode_0x00():
     assert response['var-writeable'] == False
     assert response['var-group'] == group_id
 
-def test_decode_0x00_writeable():
+def test_returned_descriptor_variable_writeable():
     distantio = distantio_protocol()
 
     # inputs
     var_id = 999
-    name = "testvar"
+    name = "testvariable 2"
     fmt = 0xF3
 
     # formatting
-    formatted_name = "{:8s}".format(name)
+    formatted_name = "{:14s}".format(name)
     v = (var_id).to_bytes(2,byteorder='big')
     n = bytes(formatted_name,'ascii')
 
@@ -72,13 +74,15 @@ def test_decode_0x00_writeable():
     assert response['var-type'] ==  fmt & 0x0F
     assert response['var-writeable'] == True
 
-def test_decode_float():
+def test_returned_value_float():
     distantio = distantio_protocol()
 
     # inputs
     var_id = 124
     fmt = 0
     value = 1.362e-12
+    extraid_1 = 1.25
+    extraid_2 = 12715
 
     # formatting
     v = (var_id).to_bytes(2,byteorder='big')
@@ -88,6 +92,8 @@ def test_decode_float():
     frame.append(0x01)
     frame += v
     frame.append(fmt)
+    frame += pack('>f',extraid_1)
+    frame += pack('>H',extraid_2)
     frame += n
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
@@ -99,18 +105,25 @@ def test_decode_float():
     assert response['var-id'] == var_id
     assert round(response['var-value'],5) == round(value,5)
     assert response['var-type'] == fmt
+    assert round(response['var-time'],5) == round(extraid_1,5)
+    assert response['var-array-index'] == extraid_2
 
-def test_decode_float_in_group():
+"""
+# Not implemented yet, probably not worth it
+def test_returned_value_float_in_group():
     distantio = distantio_protocol()
 
     # inputs
     # var id 124
     # group id 0xe
     var_id = 124
-    var_group = 0xe
-    formatted_id = ((var_group & 0x3F) << 10) + var_id
+    group_id = 0xe
+    formatted_id = ((group_id & 0x3F) << 10) + var_id
     fmt = 0
     value = 1.362e-12
+
+    extraid_1 = 2.725
+    extraid_2 = 9513
 
     # formatting
     v = (formatted_id).to_bytes(2,byteorder='big')
@@ -120,6 +133,8 @@ def test_decode_float_in_group():
     frame.append(0x01)
     frame += v
     frame.append(fmt)
+    frame += pack('>f',extraid_1)
+    frame += pack('>H',extraid_2)
     frame += n
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
@@ -129,9 +144,12 @@ def test_decode_float_in_group():
 
     assert response['type'] == 'returned-value'
     assert response['var-id'] == var_id
-    #assert response['var-group'] == 0xe
+    assert response['var-group'] == group_id
     assert round(response['var-value'],5) == round(value,5)
     assert response['var-type'] == fmt
+    assert round(response['var-time'],5) == round(extraid_1,5)
+    assert response['var-array-index'] == extraid_2
+"""
 
 def test_decode_int():
     distantio = distantio_protocol()
@@ -141,6 +159,8 @@ def test_decode_int():
     group_id = 37
     fmt = 3
     value = 126594521
+    extraid_1 = 2.725
+    extraid_2 = 9513
 
     # formatting
     v = (var_id + (group_id<<10)).to_bytes(2,byteorder='big')
@@ -150,6 +170,8 @@ def test_decode_int():
     frame.append(0x01)
     frame += v
     frame.append(fmt)
+    frame += pack('>f',extraid_1)
+    frame += pack('>H',extraid_2)
     frame += n
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
@@ -161,14 +183,18 @@ def test_decode_int():
     assert response['var-id'] == var_id
     assert response['var-value'] == value
     assert response['var-type'] == fmt
+    assert round(response['var-time'],5) == round(extraid_1,5)
+    assert response['var-array-index'] == extraid_2
 
-def test_decode_writeable_int():
+def test_returned_value_writeable_int():
     distantio = distantio_protocol()
 
     # inputs
     var_id = 124
     fmt = 0xF3
     value = 126594521
+    extraid_1 = 2.725
+    extraid_2 = 9513
 
     # formatting
     v = (var_id).to_bytes(2,byteorder='big')
@@ -178,6 +204,8 @@ def test_decode_writeable_int():
     frame.append(0x01)
     frame += v
     frame.append(fmt)
+    frame += pack('>f',extraid_1)
+    frame += pack('>H',extraid_2)
     frame += n
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
@@ -189,6 +217,8 @@ def test_decode_writeable_int():
     assert response['var-id'] == var_id
     assert response['var-value'] == value
     assert response['var-type'] == fmt & 0x0F
+    assert round(response['var-time'],5) == round(extraid_1,5)
+    assert response['var-array-index'] == extraid_2
 
 def test_decode_group_descriptor():
     distantio = distantio_protocol()
@@ -199,7 +229,7 @@ def test_decode_group_descriptor():
     name = "testvar"
 
     # formatting
-    formatted_name = "{:8s}".format(name)
+    formatted_name = "{:14s}".format(name)
     n = bytes(formatted_name,'ascii')
 
     # formatting
@@ -222,11 +252,11 @@ def test_decode_group_descriptor():
     assert response['group-name'] == formatted_name
 
 # test decoding valid alive-signal frame
-def test_decode_0x03():
+def test_alive_signal():
     distantio = distantio_protocol()
 
-    data = [0x03,0x00,0x00,0x00,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17]
-    frame = bytearray(data)
+    frame = bytearray(18)
+    frame[0] = 0x03
     crc = crc16(frame).to_bytes(2,byteorder='big')
     frame += crc
 
@@ -238,7 +268,7 @@ def test_decode_wrong_crc():
     distantio = distantio_protocol()
 
     var_id = (38153).to_bytes(2,byteorder='big')
-    n = bytes("{:8s}".format("testvar"),'ascii')
+    n = bytes("{:14s}".format("testvar"),'ascii')
 
     frame = bytearray()
     frame.append(0x00)
@@ -252,11 +282,11 @@ def test_decode_wrong_crc():
 
     assert pytest.raises(ValueError,distantio.process,frame)
 
-def test_decode_wrong_size():
+def test_returned_descriptor_wrong_size():
     distantio = distantio_protocol()
 
     var_id = (38153).to_bytes(2,byteorder='big')
-    n = bytes("{:8s}".format("testvar"),'ascii')
+    n = bytes("{:14s}".format("testvar"),'ascii')
 
     frame = bytearray()
     frame.append(0x00)
@@ -266,12 +296,12 @@ def test_decode_wrong_size():
 
     assert pytest.raises(IndexError,distantio.process,frame)
 
-def test_decode_wrong_format_0x00():
+def test_returned_descriptor_wrong_format():
     distantio = distantio_protocol()
 
     var_id = 38153
     name = "testvar"
-    formatted_name = "{:8s}".format(name)
+    formatted_name = "{:14s}".format(name)
 
     v = (var_id).to_bytes(2,byteorder='big')
     n = bytes(formatted_name,'ascii')
@@ -279,7 +309,7 @@ def test_decode_wrong_format_0x00():
     frame = bytearray()
     frame.append(0x00)
     frame += v
-    frame.append(0x09)
+    frame.append(0xFF)# Wrong format
     frame += n
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
@@ -287,12 +317,12 @@ def test_decode_wrong_format_0x00():
 
     assert pytest.raises(ValueError,distantio.process,frame)
 
-def test_decode_wrong_format_0x01():
+def test_returned_value_wrong_format():
     distantio = distantio_protocol()
 
     var_id = 38153
-    name = "testvar"
-    formatted_name = "{:8s}".format(name)
+    name = "testvariable 3"
+    formatted_name = "{:14s}".format(name)
 
     v = (var_id).to_bytes(2,byteorder='big')
     n = bytes(formatted_name,'ascii')
@@ -300,7 +330,7 @@ def test_decode_wrong_format_0x01():
     frame = bytearray()
     frame.append(0x01)
     frame += v
-    frame.append(0x09)
+    frame.append(0xFF)# Wrong format
     frame += n
 
     crc = crc16(frame).to_bytes(2,byteorder='big')
@@ -312,14 +342,14 @@ def test_decode_wrong_cmd():
     distantio = distantio_protocol()
 
     var_id = 38153
-    name = "testvar"
-    formatted_name = "{:8s}".format(name)
+    name = "testvariable 1"
+    formatted_name = "{:14s}".format(name)
 
     v = (var_id).to_bytes(2,byteorder='big')
     n = bytes(formatted_name,'ascii')
 
     frame = bytearray()
-    frame.append(0x09)
+    frame.append(0xFF)# Wrong command
     frame += v
     frame.append(0x00)
     frame += n
