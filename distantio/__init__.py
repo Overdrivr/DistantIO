410c6cb48023fc3832bb26766bdb46078668a4a5
Rémi Bèges <remi.beges@gmail.com>
2015-10-23 13:26:21 +0200

    Added more convenient pre-import

--- distantio/__init__.py
+++ distantio/__init__.py
@@ -2,4 +2,6 @@
 # For conditions of distribution and use, see copyright notice in the LICENSE file
 
 from distantio.DistantIO import DistantIO
+from distantio.DistantIOProtocol import distantio_protocol
 from distantio.SerialPort import SerialPort
+from distantio.crc import crc16
